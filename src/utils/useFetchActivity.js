8290b8cd2b282e4a23f53f6a956b5738eb42d39d
Saliou1920 <diopsaliou45@gmail.com>
2021-11-01 04:03:25 -0300

    fetching api

--- src/utils/useFetchActivity.js
+++ src/utils/useFetchActivity.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const url = 'https://aircall-job.herokuapp.com/activities';
+const url = "https://aircall-job.herokuapp.com/activities";
 export default function useFetchActivity() {
-    const [activity, setActivity] = React.useState([])
-    const [loading, setLoading] = React.useState(false);
-    const [error, setError] = React.useState(null);
+  const [activity, setActivity] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
-    React.useEffect(() => {
-        setLoading(true);
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setActivity(data);
-                setLoading(false);
-            })
-            .catch(error => {
-                setError(error);
-                setLoading(false);
-            });
-    }, []);
-
+  React.useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setActivity(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+  return [activity, setActivity, loading, error];
 }
